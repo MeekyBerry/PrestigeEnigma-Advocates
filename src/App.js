2f94cf247f4f5ef10635a95270b03033f7efe9a2
Mikky <meekyberry6@gmail.com>
2023-07-02 10:06:33 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { React, Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Fallback } from "./components/fallback";
+import Nav from "./layout/nav";
 import AppRoutes from "./routes";
+import Loader from "./assets/image/indicator.gif";
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={Fallback}>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div style={{
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+      }}><img src={Loader} alt="Loading GIF" /></div>}>
         <div className="App">
+          <Nav />
           <AppRoutes />
         </div>
       </Suspense>
